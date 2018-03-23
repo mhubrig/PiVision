c0086afa8b87a6e1677f5405ae4605f6aa3fe0e4
Mark Hubrig <mhubrig@mail.csuchico.edu>
2018-03-23 10:44:18 -0700

    Added notes for OpenCV install

--- install_bk.sh
+++ install_bk.sh
@@ -32,61 +32,6 @@
 # Rewritten and split up by Bob Tidey 
 
 #Debug enable next 3 lines
-
-
-
-#sudo apt-get install build-essential cmake pkg-config
-#sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-#sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-#sudo apt-get install libxvidcore-dev libx264-dev
-#sudo apt-get install libgtk2.0-dev
-#sudo apt-get install libatlas-base-dev gfortran
-#sudo apt-get install python2.7-dev python3-dev
-#cd ~
-#wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
-#unzip opencv.zip
-#wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
-#unzip opencv_contrib.zip
-#wget https://bootstrap.pypa.io/get-pip.py
-#sudo python get-pip.py
-#sudo pip install virtualenv virtualenvwrapper
-#sudo rm -rf ~/.cache/pip
-#export WORKON_HOME=$HOME/.virtualenvs
-#source /usr/local/bin/virtualenvwrapper.sh
-#echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.profile
-#echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.profile
-#echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
-#source ~/.profile
-#mkvirtualenv cv -p python2
-#workon cv
-#pip install numpycd ~/opencv-3.1.0/
-#mkdir build
-#cd build
-#cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
-    -D BUILD_EXAMPLES=ON
-#make -j4
-#sudo make install
-#sudo ldconfig
-#cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
-#ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 exec 5> install.txt
 BASH_XTRACEFD="5"
 set -x
